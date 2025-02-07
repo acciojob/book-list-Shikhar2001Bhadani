c8bec4da11669baba99e84c44efabae62f65088f
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-07 13:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click",function(){
+let auth = document.getElementById("author").value.trim();
+let booktitle = document.getElementById("title").value.trim();
+let isbnnum = document.getElementById("isbn").value.trim();
+
+if(booktitle && auth && isbnnum){
+	const booklist = document.getElementById("book-list");
+	const row = document.createElement('tr');
+	row.innerHTML=`
+	<td>${booktitle}</td>
+	<td>${auth}</td>
+	
+	<td>${isbnnum}</td>
+	<td><button class="delete">X</button></td>`;
+
+	booklist.appendChild(row);
+	document.getElementById("auth").value='';
+	document.getElementById("booktitle").value='';
+	document.getElementById("isbnnum").value='';
+}
+
+});
+document.getElementById("book-list").addEventListener("click",function(event){
+if(event.target.classList.contains('delete')){
+event.target.parentElement.parentElement.remove();
+}
+});
